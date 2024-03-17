8a562b7f35334e22efa50e6c521053c051f23de6
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-03-17 11:59:13 +0530

    frontend: Routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
 	return (
-		<div>
-			<Navbar />
-		</div>
+		<Routes>
+			<Route path="/" element={<Navbar />}>
+				<Route path="signin" element={<h1>Sign in</h1>} />
+				<Route path="signup" element={<h1>Sign up</h1>} />
+			</Route>
+		</Routes>
 	);
 }
 
