5a9e9baa5bacd33811dbe234542365d90d3c771f
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-03-18 12:18:14 +0530

    backend: zod validation for signup route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import { z } from "zod";
 import "dotenv/config";
+import bcrypt from "bcrypt";
 
 const app = express();
 let PORT = 3000;
@@ -11,9 +13,37 @@ mongoose.connect(process.env.DB_LOCATION, {
 	autoIndex: true,
 });
 
-// testing
+const signUpSchema = z.object({
+	fullname: z
+		.string()
+		.min(3, { message: "Username must be at least 3 characters" }),
+	email: z
+		.string()
+		.min(1, { message: "Email is required" })
+		.email("Invalid email address"),
+	password: z
+		.string()
+		.min(6, { message: "Password must be at least 6 characters" }),
+});
+
 app.post("/signup", (req, res) => {
-	res.json(req.body);
+	const { fullname, email, password } = req.body;
+
+	try {
+		signUpSchema.parse({ fullname, email, password });
+
+		bcrypt.hash(password, 10, (err, hashed_password) => {
+			console.log(hashed_password);
+		});
+
+		return res.status(200).json({
+			"status": "OK",
+		});
+	} catch (error) {
+		return res.status(403).json({
+			"error": error,
+		});
+	}
 });
 
 app.listen(PORT, () => {
