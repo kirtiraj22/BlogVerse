c0f1c3c05436768c7f206d7bc72bbc994315a0d1
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-04-26 12:13:21 +0530

    frontend: customized blog title

--- client/src/components/BlogEditor.jsx
+++ client/src/components/BlogEditor.jsx
@@ -3,12 +3,40 @@ import { Link } from "react-router-dom";
 import AnimationWrapper from "../common/PageAnimation";
 import logo from "../images/logo.png"
 import defaultBanner from "../images/banner.png"
+import toast, { Toaster } from "react-hot-toast";
 const BlogEditor = () => {
     const handleBannerUpload = (e) => {
-        console.log(e)
         let img = e.target.files[0]
-        console.log(img)
+		if(img){
+			// let loadingToast = toast.loading("Uploading...")
+			// uploadImage(img).then((url)=>{
+			// 	if(url){
+			// 		toast.dismiss(loadingToast);
+			// 		toast.success("Uploaded successfully");
+			// 		blogBannerRef.current.src = url
+			// 	}
+			// }).catch(err => {
+			// 	toast.dismiss(loadingToast)
+			// 	return toast.error(err);
+			// })
+
+			console.log(img)
+		}
     }
+
+	const handleTitleKeyDown = (e) => {
+		if(e.keyCode === 13){
+			e.preventDefault();
+		}
+	}
+
+	const handleTitleChange = (e) => {
+		let input = e.target;
+
+		input.style.height = "auto";
+		input.style.height = input.scrollHeight + "px";
+	}
+
 	return (
 		<>
 			<nav className="navbar">
@@ -24,6 +52,7 @@ const BlogEditor = () => {
 					<button className="btn-light py-2">Save Draft</button>
 				</div>
 			</nav>
+			<Toaster />
 			<AnimationWrapper>
 				<section>
 					<div className="mx-auto max-w-[900px] w-full">
@@ -42,6 +71,14 @@ const BlogEditor = () => {
                                 />
                             </label>
                         </div>
+						<textarea 
+						placeholder="Blog Title"
+						className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+						onKeyDown={handleTitleKeyDown}
+						onChange={handleTitleChange}
+						>
+
+						</textarea>
                     </div>
 				</section>
 			</AnimationWrapper>
